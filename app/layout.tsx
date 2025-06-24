4b8e5c2f836b89c50b75504d074d58e24105f7d7
Ajay Guliya <83868323+arjunguliya@users.noreply.github.com>
2025-06-24 18:51:05 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,11 +7,11 @@ import bgImage from "../public/new-bg.png";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
-let title = "Llama Tutor – AI Personal Tutor";
-let description = "Learn faster with our open source AI personal tutor";
-let url = "https://llamatutor.com/";
-let ogimage = "https://llamatutor.together.ai/og-image.png";
-let sitename = "llamatutor.com";
+let title = "Scholar AI - Personal Tutor";
+let description = "Learn faster with Scholar AI's Personal Tutor";
+let url = "https://aitutor.scholarai.co/";
+let ogimage = "https://aitutor.scholarai.co/og-image.png";
+let sitename = "scholarai.co";
 
 export const metadata: Metadata = {
   metadataBase: new URL(url),
@@ -45,7 +45,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full">
       <head>
-        <PlausibleProvider domain="llamatutor.together.ai" />
+        <PlausibleProvider domain="aitutor.scholarai.co" />
       </head>
 
       <body
